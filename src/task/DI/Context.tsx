809c6181d67f14323b0628cbd522942b052da970
Max Ulshin <max.ulshin.max@istat.com.ua>
2021-07-22 23:39:07 +0300

    delete di

--- src/task/DI/Context.tsx
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-// import { interfaces } from "inversify";
-
-// const context = React.createContext<interfaces.Container | null>(null);
-
-// export default context;
-// export const
-export {};
